8925b9977a37baee78a32dd540b43a8a460ae1af
Koriandr <ofortnik@mail.ru>
2019-11-12 12:27:47 +0500

    WKP-11 Переделал 404.php

--- 404.php
+++ 404.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+    <title><?php _e( 'Page Not Found', 'wikipress' ); ?></title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat%3A400%2C500%2C700&display=swap&subset=cyrillic&ver=5.2.3">
@@ -8,18 +9,18 @@
 </head>
 <body>
     <div class="wrap">
-        <div class="col img"><img src="<?php echo get_template_directory_uri() . '/img/404.svg'; ?>"></div>
-        
-        <div class="col txt">
-            <div class="logo"><img src="<?php echo get_template_directory_uri() . '/img/logo-footer.png'; ?>" alt="Логотип"></div>
-            <div class="txt-item-1"><b>Не переживайте, все в порядке =) просто страница не нашлась...</b></div>
-            <div class="txt-item-2">Такое иногда бывает) Наверное, кто-то где-то ошибся...</div>
-            <div class="txt-btn"><a href="/">Перейти на главную</a></div>
-            <div class="txt-item-2">Вы также можете сообщить нам об этом недоразумении по телефону:</div>
-            <div class="txt-tlf"><a href="tel:+73433456465"><b>+7 (343) 3-456-465</b></a></div>
+        <div class="txt">
+            <div class="logo">
+                <img src="<?php echo get_template_directory_uri() . '/img/logo-footer.png'; ?>" 
+                     alt="<?php esc_attr_e( 'Logo', 'wikipress' ); ?>">
+            </div>
+            <div class="txt-item-1">
+                <b><?php _e( 'Sorry, the page you tried to view does not exist.', 'wikipress' ); ?></b>
+            </div>
+            <div class="txt-btn">
+                <a href="/"><?php _e( 'Go to Home page', 'wikipress' ); ?></a>
+            </div>
         </div>
     </div>
-    
-    <div class="bottom">© Jumpnet, Разработка веб-сайтов. 2019. Все права защищены.</div>
 </body>
 </html>
